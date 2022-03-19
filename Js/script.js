e3ad82d1187c06f2805cca197068982bd5c52946
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-03-19 13:21:33 +0300

    style:body-add js style to ordernow section

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,24 @@
+
+ function Orders(size, crust, toppings, number, total){
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.number = number;
+        this.total = total
+    }
+
+
 $("button#btn").click(function(event){
-    let size = $("#size option:selected").val();
-    let crust = $("#crust option:selected").val();
-    let toppings = $("#toppings option:selected").val();
-
-
-
-    $("button#btn").hide();
-    
-});
-
-
+    var size = $("#size option:selected").val();
+    var crust = $("#crust option:selected").val();
+    var toppings = $("#toppings option:selected").val();
+    var number = $("#number option:selected").val();
+  
+    var newOrder =  new Orders (size, crust, toppings, number, total)
+    $("button#delivery").show()
+    $("button.location").slideDown(1000);
+   
+}); 
 function getSizeCost() {
     var selectedSize = document.getElementById("size").value;
     return parseInt(selectedSize);
@@ -33,4 +42,13 @@ btn.addEventListener("click", (event) => {
     
     var totalPrice = ((getSizeCost() + getCrustCost() + getToppingsCost()) * getNumber());
     alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." )
+
+
+
+//  $("#ordersmade").append('<tr><td id="size">'+ getSizeCost().size + '</td><td id="crust">' + getCrustCost().crust + '</td><td id="toppings">'+getToppingsCost.toppings + '</td><td id="number">'+getNumber().number +'</td><td id="total">'+totalPrice.total+'</td></tr>');
+ 
+   
+   $("#ordermade").append("Your bill is sh. " + getNumber() + " Pizzza  suming up to" + " " +totalPrice + ".");
+   $("ul#youorder").append("<li><span class='youorder'>" + selectedSize.getSizeCost() + "</span></li>");
+   
 })
